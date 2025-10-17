0889d0a997885ed01a259628134a07f481c0857b
Adam <adam@lassiter.uk>
2025-10-17 16:56:15 +0100

    Add images and constants

---
+++ constants/armorsizes.js
@@ -0,0 +1,32 @@
+const ARMOR_SIZES = [
+  {
+    displayName: "Light Armor",
+    type: "Equipment",
+    category: "armor",
+    tags: ["ArmorSize"],
+    warbondCode: "",
+    internalName: "light",
+    imageURL: "",
+    tier: "b",
+  },
+  {
+    displayName: "Medium Armor",
+    type: "Equipment",
+    category: "armor",
+    tags: ["ArmorSize"],
+    warbondCode: "",
+    internalName: "medium",
+    imageURL: "",
+    tier: "b",
+  },
+  {
+    displayName: "Heavy Armor",
+    type: "Equipment",
+    category: "armor",
+    tags: ["ArmorSize"],
+    warbondCode: "",
+    internalName: "heavy",
+    imageURL: "",
+    tier: "b",
+  },
+];
