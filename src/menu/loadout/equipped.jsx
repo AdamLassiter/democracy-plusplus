3f04e24202b4c7e23d21f21e64b1ca282eaf3c6e
Adam <adam@lassiter.uk>
2025-10-24 22:31:32 +0100

    Match tabs to inventory slots

--- src/menu/loadout/equipped.jsx
+++ src/menu/loadout/equipped.jsx
@@ -26,12 +26,12 @@ export default function Equipped() {
     <Typography variant="h5">Equipment</Typography>
     <Grid direction="column" container spacing={1}>
       <Grid direction="row" container spacing={1}>
+        {armorPassive ? <ItemDisplay item={getConstant(armorPassive)} onClick={() => unequip(armorPassive)} /> : <MissingArmor />}
+        {booster ? <ItemDisplay item={getConstant(booster)} onClick={() => unequip(booster)} /> : <MissingBooster />}
+        <Divider orientation="vertical" variant="middle" flexItem />
         {primary ? <ItemDisplay item={getConstant(primary)} onClick={() => unequip(primary)} /> : <MissingPrimary />}
         {secondary ? <ItemDisplay item={getConstant(secondary)} onClick={() => unequip(secondary)} /> : <MissingSecondary />}
         {throwable ? <ItemDisplay item={getConstant(throwable)} onClick={() => unequip(throwable)} /> : <MissingThrowable />}
-        <Divider orientation="vertical" variant="middle" flexItem />
-        {armorPassive ? <ItemDisplay item={getConstant(armorPassive)} onClick={() => unequip(armorPassive)} /> : <MissingArmor />}
-        {booster ? <ItemDisplay item={getConstant(booster)} onClick={() => unequip(booster)} /> : <MissingBooster />}
         <Divider orientation="vertical" variant="middle" flexItem />
         {stratagems.map(stratagem => stratagem ? <ItemDisplay item={getConstant(stratagem)} onClick={() => unequip(stratagem)} /> : <MissingStratagem />)}
       </Grid>
